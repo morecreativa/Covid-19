58a435b6cd91c281affa9917af87111e6aa98e07
morecreativa <59593893+morecreativa@users.noreply.github.com>
2020-09-01 20:04:25 +0900

    Updates

---
+++ precache-manifest.52e6127ff3600dd4243ea3a007682c25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ea44c2c68151eecfe667b01d709e5b",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "21d1503dd8cb69c49d18",
+    "url": "/Covid-19/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "36c61fe84d0c36fefdbe",
+    "url": "/Covid-19/static/css/main.1c631718.chunk.css"
+  },
+  {
+    "revision": "21d1503dd8cb69c49d18",
+    "url": "/Covid-19/static/js/2.9835d1e9.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/Covid-19/static/js/2.9835d1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c61fe84d0c36fefdbe",
+    "url": "/Covid-19/static/js/main.f156b43f.chunk.js"
+  },
+  {
+    "revision": "0425b84ba095d6b8082c",
+    "url": "/Covid-19/static/js/runtime-main.c39ec8b8.js"
+  }
+]);
