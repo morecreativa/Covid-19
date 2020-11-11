cc4529599f80e2f74dd7ae25eb6ae32cd09d7c7b
Seo Hyun Park <morecreativa@naver.com>
2020-11-11 22:20:10 +0900

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -20,3 +20,17 @@ function Map({countries, casesType, center,zoom}) {
 }
 
 export default Map
+
+function Map2({countries, casesType, center, zoom}){
+    return (
+        <div className="Map">
+            <LeafletMap center={center} zoom={zoom}>
+                <TileLayer
+                url=https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                {showDataOnMap(countries, casesType)}
+            </LeafletMap>
+        </div>
+    )
+}
